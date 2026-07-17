f680a1443d6b8a0ca6bcd9a606a7665f88b6e9e8
Jheison <jeisons803@gmail.com>
2026-07-17 16:17:49 -0500

    Organiza proyectos de desarrollo web en carpetas

---
+++ proyecto-final/script.js
@@ -0,0 +1,11 @@
+// Mode Dark, light
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle("night-mode");
+  }
+
+  function msgmodal(String msg){
+    margin:AuthenticatorAssertionResponse;
+
+  }
+  
